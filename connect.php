0152c5749742b73fe24c302cab176bc17fe7b507
Carlos Ortega <67563350+master203040@users.noreply.github.com>
2021-08-21 22:00:38 -0500

    modificacion de la condicional

--- connect.php
+++ connect.php
@@ -1,18 +1,18 @@
 <?php
 
 
-    $num1=$_GET['n1'];
-    $num2 =$_GET['n2'];
-    $option = $_GET['op'];
+    $n1=$_GET['n1'];
+    $n2 =$_GET['n2'];
+    $op = $_GET['op'];
     $outcome = 0;
-    if($option=="1"){
-        $outcome = $num1+$num2;
-    }elseif ($option == "2") {
-        $outcome= $num1-$num2;
-    }elseif ($option =="3"){
-      $outcome = $num1*$num2;  
-    }elseif ($option=="4") {
-        $outcome = $num1/$num2;
+    if($op=="sum"){
+        $outcome = $n1+$n2;
+    }elseif ($op == "subtraction") {
+        $outcome= $n1-$n2;
+    }elseif ($op =="multiplication"){
+      $outcome = $n1*$n2;  
+    }elseif ($op =="division") {
+        $outcome = $n1/$n2;
     }
 
     echo"el resultado es : ".$outcome;
