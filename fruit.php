202c10baca2b0eb7ab445060c4734e62dd18e1f2
Carlos Ortega <67563350+master203040@users.noreply.github.com>
2021-08-31 21:57:40 -0500

    creation for view

---
+++ fruit.php
@@ -0,0 +1,18 @@
+<?php
+
+$fruit1 = $_POST["fruit1"];
+$fruit2 = $_POST["fruit2"];
+$fruit3 = $_POST["fruit3"];
+$fruit4 = $_POST["fruit4"];
+$fruit5 = $_POST["fruit5"];
+
+try {
+    
+    
+    
+} catch (\Throwable $th) {
+    //throw $th;
+}
+
+
+?>
