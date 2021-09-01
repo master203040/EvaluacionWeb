ae62b699b911084b1b619fcefc2bf5391c3c0053
Carlos Ortega <67563350+master203040@users.noreply.github.com>
2021-08-31 23:01:03 -0500

    second view html y php

--- fruit.php
+++ fruit.php
@@ -1,15 +1,18 @@
 <?php
 
-$fruit1 = $_POST["fruit1"];
-$fruit2 = $_POST["fruit2"];
-$fruit3 = $_POST["fruit3"];
-$fruit4 = $_POST["fruit4"];
-$fruit5 = $_POST["fruit5"];
+$opt1 = $_POST["opt1"];
+$opt2 = $_POST["opt2"];
+$opt3 = $_POST["opt3"];
+$opt4 = $_POST["opt4"];
+$opt5 = $_POST["opt5"];
 
 try {
     
-    
-    
+    if( $opt1 == "Papaya" && $opt2 == "Pineapple" && $opt3 == "Mango" &&  $opt4 == "Strawberry" && $opt5 == "Vanilla ice Cream"){
+
+        echo ("la primera fruta es :".$opt1.)
+    }
+
 } catch (\Throwable $th) {
     //throw $th;
 }
