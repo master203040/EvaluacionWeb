a9a4b72350568956fce08b9eacf83855cbd4f957
Carlos Ortega <67563350+master203040@users.noreply.github.com>
2021-09-01 20:48:04 -0500

    se realizan cambios en la estructura del html

--- fruit.php
+++ fruit.php
@@ -6,15 +6,10 @@ $opt3 = $_POST["opt3"];
 $opt4 = $_POST["opt4"];
 $opt5 = $_POST["opt5"];
 
-try {
-    
-    if( $opt1 == "Papaya" && $opt2 == "Pineapple" && $opt3 == "Mango" &&  $opt4 == "Strawberry" && $opt5 == "Vanilla ice Cream"){
 
-        echo ("la primera fruta es :".$opt1.)
-    }
+if($opt1 == "papaya"){
 
-} catch (\Throwable $th) {
-    //throw $th;
+    echo "fruta".$opt1;
 }
 
 
