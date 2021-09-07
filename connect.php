ea0343a2ec6086d968b6d1bde22f5f1432e64b56
Carlos Ortega <67563350+master203040@users.noreply.github.com>
2021-09-06 21:45:30 -0500

    Second update

--- connect.php
+++ connect.php
@@ -1,28 +1,33 @@
 <?php
 
-    $num1=$_POST['n1'];
-    $num2 =$_POST['n2'];
+    $number1=$_POST['n1'];
+    $number2 =$_POST['n2'];
     $op =$_POST['op'];
-    
+
+
     if ($op== "sum"){
 
-        $outcome = $num1+$num2;
-        echo "la suma de ".$num1."+".$num2."=".$outcome;
+        $outcome = $number1+$number2;
+        echo "la suma de ".$number1."+".$number2."=".$outcome;
+        
     }elseif($op=="subtraction"){
 
-        $outcome = $num1-$num2;
-        echo "la resta de".$num1."-",$num2."=".$outcome;
+        $outcome = $number1-$number2;
+        echo "la resta de".$number1."-",$number2."=".$outcome;
+        
     }elseif($op =="multiplication"){
 
-        $outcome = $num1*$num2;
-        echo"la multiplicacion de ".$num1."*".$num2."=".$outcome;
+        $outcome = $number1*$number2;
+        echo"la multiplicacion de ".$number1."*".$number2."=".$outcome;
 
     }elseif($op =="division"){
 
-        $outcome = $num1/$num2;
-        echo "la division de".$num1."/".$num2."=".$outcome;
-    }
-
+        $outcome = $number1/$number2;
+        echo "la division de".$number1."/".$number2."=".$outcome;
+    }   
+    
+    
+    
 
     
 
