c08dbddcdd77d32e73bcb482fa07519e55c16f23
Carlos Ortega <67563350+master203040@users.noreply.github.com>
2021-09-08 22:30:39 -0500

    Update fruit.php

--- fruit.php
+++ fruit.php
@@ -1,21 +1,25 @@
 
 <?php
 
+
+
     $fruit = array(
+        
+        array('name1'=>"pineapple",'foto'=>""),
+        array('name2'=>"papaya",'foto'=>""),
+        array('name3'=>"watermelon",'foto'=>""),
+        array('name4'=>"mango",'foto'=>""),
+        array('name5'=>"apple",'foto'=>"")
+        
 
-        'usuario 1:' => array('name1'=>"pineapple",'foto'=>""),
-        'usuario 2:' => array('name2'=>"papaya",'foto'=>""),
-        'usuario 3:' => array('name3'=>"watermelon",'foto'=>""),
-        'usuario 4:' => array('name4'=>"mango",'foto'=>""),
-        'usuario 5:' => array('name5'=>"apple",'foto'=>"")
+      
     );
-
-    foreach ($fruit as $usuario => $info) {
-        
-        echo('<strong>'.$usuario.'</strong><br>');
-
-        
+    foreach ($fruit as $fruit) {
+           
+        echo('<strong>'.$fruit.'</strong><br>');
     }
+
+    
     
     
    
