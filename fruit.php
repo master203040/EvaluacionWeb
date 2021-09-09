ff1b2fb2d0306fb56a2db181d559dd041e8bba32
Carlos Ortega <67563350+master203040@users.noreply.github.com>
2021-09-08 22:03:57 -0500

    programando un multidireccional

--- fruit.php
+++ fruit.php
@@ -1,11 +1,25 @@
+
 <?php
 
-    $fr1 = $_POST["fruit1"];
-    $fr2 = $_POST["fruit2"];
-    $fr3 = $_POST["fruit3"];
-    $fr4 = $_POST["fruit4"];
-    $fr5 = $_POST["fruit5"];
+    $fruit = array(
+
+        'usuario 1:' => array('name1'=>"pineapple",'foto'=>""),
+        'usuario 2:' => array('name2'=>"papaya",'foto'=>""),
+        'usuario 3:' => array('name3'=>"watermelon",'foto'=>""),
+        'usuario 4:' => array('name4'=>"mango",'foto'=>""),
+        'usuario 5:' => array('name5'=>"apple",'foto'=>"")
+    );
+
+    foreach ($fruit as $usuario => $info) {
+        
+        echo('<strong>'.$usuario.'</strong><br>');
+
+        
+    }
     
     
    
+
+
+
 ?> 
