f50015e62a1eeba1785e28c63d8a39b03ccef47b
Carlos Ortega <67563350+master203040@users.noreply.github.com>
2021-09-11 17:12:11 -0500

    edit

---
+++ fruit.php
@@ -0,0 +1,21 @@
+<?php
+
+if (isset($_POST["send"])){
+
+    $fruit = array(
+                
+        array('name'=>"nombreFruit1",'foto1'=>""),
+        array('name'=>"nombreFruit2",'foto'=>""),
+        array('name'=>"nombreFruit3",'foto'=>""),
+        array('name'=>"nombreFruit4",'foto'=>""),
+        array('name'=>"nombreFruit5",'foto'=>"")
+        
+    
+    
+    );
+
+}
+
+
+
+?>
