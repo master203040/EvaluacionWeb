518c427bbc2ad718418272d21b56b51579bac806
Carlos Ortega <67563350+master203040@users.noreply.github.com>
2021-09-11 21:23:57 -0500

    ajuste mass / view2

--- mass.php
+++ mass.php
@@ -1,4 +1,16 @@
-<?php
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    </head>
+        <body class="bg-success p-2 text-dark bg-opacity-25">
+            
+        <?php
 
             if (isset($_POST["calcular"])){
 
@@ -8,7 +20,7 @@
 
                 $imc = $peso/($altura* $altura);
 
-                echo ("its IMC is from ".$imc);
+               
 
                 if($imc < 18.5){
 
@@ -19,11 +31,11 @@
                     echo(" the weigth it is normal <bre>");
 
                 }else if ($imc >= 25 && $imc <= 29.9 ) {
-                    
-                    echo(" over weigth it is grade one ");
+                            
+                echo(" over weigth it is grade one ");
 
                 }else if ($imc >= 27 && $imc <= 29.9) {
-                    
+                            
                     echo(" over weigth it is grade two ");
 
                 }else if ($imc >= 30 && $imc <= 34.9  ){
@@ -31,15 +43,15 @@
                     echo (" obese one "); 
 
                 }else if ($imc >=35 && $imc <= 39.9){
-                
+                        
                     echo (" obese one "); 
 
                 }else if ($imc >= 40 && $imc <= 49.9){
-                    
+                            
                     echo (" obese two "); 
 
                 }else if ($imc >= 50) {
-                    
+                            
                     echo (" obese threen "); 
                 }else{
 
@@ -51,3 +63,25 @@
 
 
         ?>
+        
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row g-0  d-flex justify-content-center">
+                <div class="col-md-4">
+                   <img src="Gym.jpg" class="img-fluid rounded-start" alt="foto">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class = "card-title text-dark text-center fw-bolder"> IMC</h5>
+                        <h5 class="card-title text-dark text-center fw-bolder"><?php echo($imc)?></h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-10 text-star">
+            <a href="view2.html" class="link-primary">To Return</a>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>     
+    </body>
+</html>
+
+
