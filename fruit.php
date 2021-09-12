37932e9d6fa098d97945acf9a48f2f9c30bc20e2
Carlos Ortega <67563350+master203040@users.noreply.github.com>
2021-09-11 21:56:27 -0500

    CAMBIO DE TITILO

--- fruit.php
+++ fruit.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PhpFrutas</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
 </head>
 <body class = "bg-success p-2 text-dark bg-opacity-25">
@@ -34,7 +34,10 @@ if(isset($_POST["send"])){
                         <h5 class="card-title text-dark text-center fw-bolder"><?php echo($fruta["name"])?></h5>
                     </div>
                 </div>
-             </div>
+            </div>
+            <div class="col-md-10 text-center">
+              <a href="view.html" class="link-primary">To Return</a>
+            </div>
         <?php endif ?>
     <?php endforeach ?>
 <?php endif ?>
